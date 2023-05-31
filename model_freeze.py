28967332746ee7551e31742170f7f70dde53920d
Arav Dhoot <aravdhoot@gmail.com>
2023-05-31 19:20:17 +0400

    adding model freezing file

---
+++ model_freeze.py
@@ -0,0 +1,9 @@
+
+class ModelFreeze():
+    def __init__(self, model, task, training_type):
+        self.model = model
+        self.task = task
+        self.training_type = training_type
+
+
+    
