3e16bb7b6c8768038ecd5151fa1f73745abe599a
Arav Dhoot <aravdhoot@gmail.com>
2023-06-03 17:46:38 +0400

    increasing batch_size

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def run_experiment (
 
         dataset = load_dataset('glue', 'qqp')
         num_classes = 2
-        batch_size = 120
+        batch_size = 240
 
         train_data = dataset['train']
         test_data = dataset['validation']
