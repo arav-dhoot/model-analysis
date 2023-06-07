c8e1cb6875ec47b38f9c627c505dc2934a4e61e7
Arav Dhoot <aravdhoot@gmail.com>
2023-06-07 21:25:30 +0400

    adding the stsb task dataset class

---
+++ dataset/stsbdataset.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import Dataset
+
+class STSBenchmarkDataset(Dataset):
+    def __init__(self, data, tokenizer, max_length=128):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        item = self.data[index]
+        sentence1 = item['sentence1']
+        sentence2 = item['sentence2']
+        similarity_score = item['similarity_score']
+
+        encoded_inputs = self.tokenizer.encode_plus(
+            sentence1,
+            sentence2,
+            max_length=self.max_length,
+            truncation=True,
+            padding='max_length',
+            return_tensors='pt'
+        )
+
+        input_ids = encoded_inputs['input_ids'].squeeze()
+        attention_mask = encoded_inputs['attention_mask'].squeeze()
+
+        return {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'similarity_score': torch.tensor(similarity_score)
+        }
