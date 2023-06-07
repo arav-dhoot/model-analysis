95e71c1bb6f3e0dae50c842f240aaef1b91c8e5a
Arav Dhoot <aravdhoot@gmail.com>
2023-06-07 21:27:50 +0400

    adding support for the stsb task in the main file

--- dataset/stsbdataset.py
+++ dataset/stsbdataset.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import Dataset
 
-class STSBenchmarkDataset(Dataset):
+class STSBDataset(Dataset):
     def __init__(self, data, tokenizer, max_length=128):
         self.data = data
         self.tokenizer = tokenizer
