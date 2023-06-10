8e1bef2955e59ef7d128d64a7e9a680b017875cc
Arav Dhoot <aravdhoot@gmail.com>
2023-06-10 15:18:32 +0400

    changing the batch size for qnli

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def run_experiment (
 
         dataset = load_dataset('glue', 'qnli')
         num_classes = 2
-        batch_size = 250
+        batch_size = 200
 
         train_data = dataset['train']
         test_data = dataset['validation']
