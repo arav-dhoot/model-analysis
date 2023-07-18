5833c1105eb67d424255fe91023aff0d6c10bd96
Arav Dhoot <aravdhoot@gmail.com>
2023-07-18 11:28:42 +0530

    removing cache and adding all files

---
+++ main.py
@@ -0,0 +1,215 @@
+import time
+import torch
+import wandb
+import random
+from model import Model
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from torch.utils.data import DataLoader
+
+def run_experiment (
+        model,
+        task,
+        training_type,
+        epochs, 
+        log_to_wandb,
+        learning_rate, 
+        num_classes, 
+        batch_size,
+        dropout,
+        max_tokens,
+        weight_decay, 
+        betas,
+        eps,
+        lr_scheduler, 
+        scheduler_updates
+):
+
+    torch.set_default_dtype(torch.float16)
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    task = task
+    training_type = training_type
+    log_to_wandb = log_to_wandb
+    epochs = epochs
+    learning_rate = learning_rate
+    batch_size = batch_size
+    num_classes = num_classes
+    max_tokens = max_tokens
+
+    short_name = str(random.randint(int(1e5), int(1e6) - 1))
+    run_name = f'{task}-{training_type}-{short_name}'
+
+    if log_to_wandb:
+        config = dict(
+            short_name=short_name,
+            run_name=run_name,
+            task=task,
+            training_type=training_type,
+            epochs=epochs,
+            learning_rate=learning_rate
+        )
+
+        wandb.init(
+            project='model_analysis',
+            group=f'{time.strftime("%m-%d", time.localtime(time.time()))}-{task}',
+            name=f'{task}-{training_type}-{short_name}',
+            config=config,
+        )
+
+    if task == 'sst2':
+        from dataset.sst2dataset import SST2Dataset
+
+        dataset = load_dataset('glue', 'sst2')
+        num_classes = num_classes
+        batch_size = batch_size
+        max_tokens = max_tokens
+
+        train_data = dataset['train']
+        test_data = dataset['validation']
+        train_dataset = SST2Dataset(train_data, tokenizer)
+        test_dataset = SST2Dataset(test_data, tokenizer)
+
+    if task == 'qqp':
+        from dataset.qqpdataset import QQPDataset
+
+        dataset = load_dataset('glue', 'qqp')
+        num_classes = num_classes
+        batch_size = batch_size
+
+        train_data = dataset['train']
+        test_data = dataset['validation']
+        train_dataset = QQPDataset(train_data, tokenizer)
+        test_dataset = QQPDataset(test_data, tokenizer)
+
+    if task == 'cola':
+        from dataset.coladataset import COLADataset
+
+        dataset = load_dataset('glue', 'cola')
+        num_classes = num_classes
+        batch_size = batch_size
+
+        train_data = dataset['train']
+        test_data = dataset['validation']
+        train_dataset = COLADataset(train_data, tokenizer)
+        test_dataset = COLADataset(test_data, tokenizer)
+
+    if task == 'wnli':
+        from dataset.wnlidataset import WNLIDataset
+
+        dataset = load_dataset('glue', 'wnli')
+        num_classes = num_classes
+        batch_size = batch_size
+
+        train_data = dataset['train']
+        test_data = dataset['validation']
+        train_dataset = WNLIDataset(train_data, tokenizer)
+        test_dataset = WNLIDataset(test_data, tokenizer)
+
+    if task == 'stsb':
+        from dataset.stsbdataset import STSBDataset
+
+        dataset = load_dataset('glue', 'stsb')
+        num_classes = num_classes
+        batch_size = batch_size
+
+        train_data = dataset['train']
+        test_data = dataset['validation']
+        train_dataset = STSBDataset(train_data, tokenizer)
+        test_dataset = STSBDataset(test_data, tokenizer)
+
+    if task == 'rte':
+        from dataset.rtedataset import RTEDataset
+
+        dataset = load_dataset('glue', 'rte')
+        num_classes = num_classes
+        batch_size = batch_size
+
+        train_data = dataset['train']
+        test_data = dataset['validation']
+        train_dataset = RTEDataset(train_data, tokenizer)
+        test_dataset = RTEDataset(test_data, tokenizer)
+    
+    if task == 'mrpc':
+        from dataset.mrpcdataset import MRPCDataset
+
+        dataset = load_dataset('glue', 'mrpc')
+        num_classes = num_classes
+        batch_size = batch_size
+
+        train_data = dataset['train']
+        test_data = dataset['validation']
+        train_dataset = MRPCDataset(train_data, tokenizer)
+        test_dataset = MRPCDataset(test_data, tokenizer)
+
+    if task == 'qnli':
+        from dataset.qnlidataset import QNLIDataset
+
+        dataset = load_dataset('glue', 'qnli')
+        num_classes = num_classes
+        batch_size = batch_size
+
+        train_data = dataset['train']
+        test_data = dataset['validation']
+        train_dataset = QNLIDataset(train_data, tokenizer)
+        test_dataset = QNLIDataset(test_data, tokenizer)
+
+    if task == 'mnli':
+        from dataset.mnlidataset import MNLIDataset
+
+        dataset = load_dataset('glue', 'mnli')
+        num_classes = num_classes
+        batch_size = batch_size
+
+        train_data = dataset['train']
+        test_data = dataset['validation_matched']
+        train_dataset = MNLIDataset(train_data, tokenizer)
+        test_dataset = MNLIDataset(test_data, tokenizer)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'mps')
+
+    model = Model(num_classes=num_classes, task=task, training_type=training_type, dropout=dropout).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=betas, eps=eps, weight_decay=weight_decay)
+    wandb.watch(model, log='all')
+
+    for epoch in range(epochs):
+        train_loss, train_accuracy, train_loss_list, train_accuracy_list, train_time_list, train_step_list = model.train_epoch(train_dataloader, optimizer, lr_scheduler, scheduler_updates, device)
+        test_loss, test_accuracy, test_loss_list, test_accuracy_list, test_time_list, test_step_list = model.test_epoch(test_dataloader, device)
+
+        for tr_loss, tr_accuracy, tr_time, tr_step in zip(train_loss_list, train_accuracy_list, train_time_list, train_step_list):
+            wandb.log(
+                {
+                    'Train Loss':tr_loss,
+                    'Train Accuracy':tr_accuracy,
+                    'Train Time':tr_time,
+                    'Train Step': (epoch * len(train_step_list)) + tr_step
+                }, 
+            )
+        
+        for te_loss, te_accuracy, te_time, te_step in zip(test_loss_list, test_accuracy_list, test_time_list, test_step_list):
+            wandb.log(
+                {
+                    'Test Loss (batch)':te_loss,
+                    'Test Accuracy (batch)':te_accuracy,
+                    'Test Time':te_time,
+                    'Test Step': (epoch * len(test_step_list)) + te_step 
+                },   
+            )
+
+        wandb.log(
+            {
+                    'Epoch':epoch + 1,
+                    'Test Accuracy (epoch)':test_accuracy,
+                    'Test Loss (epoch)':test_loss,
+                },  
+        )
+ 
+        print(f'Epoch {epoch + 1} - Train Loss: {train_loss:.4f} - Train Accuracy: {train_accuracy:.4f} - Test Loss: {test_loss:.4f} - Test Accuracy: {test_accuracy:.4f}')
+    print(f'{model.trained_proportion * 100}% of the model was trained')
+
+    if training_type == 'finetuned': 
+        file_location = model.file_write()
+        wandb.save(file_location)
+    wandb.finish()
---
+++ model.py
@@ -0,0 +1,228 @@
+import json
+import tqdm
+import time
+import torch
+import numpy as np
+import torch.nn as nn
+from transformers import AutoModel
+
+class Model(nn.Module):
+    def __init__(self, 
+                 num_classes,
+                 task,
+                 training_type,
+                 dropout,
+                 model='roberta-base', 
+                ):
+        
+        torch.set_default_dtype(torch.float16)
+        super(Model, self).__init__()
+        self.model = AutoModel.from_pretrained(model)
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Sequential(
+            nn.Linear(self.model.config.hidden_size, self.model.config.hidden_size),
+            nn.GELU(),
+            self.dropout,
+            nn.Linear(self.model.config.hidden_size, num_classes)
+            )
+        # self.fc = nn.Linear(self.model.config.hidden_size, num_classes)
+        self.name_list = [name for name, params in self.model.named_parameters()]
+        self.grad_dict = dict()
+        for name in self.name_list: self.grad_dict[name] = list()
+        self.training_type = training_type
+        self.task = task
+        self.trainable_params = 0
+        for name, param in self.model.named_parameters(): self.trainable_params += torch.numel(param)
+        self.trained_parameters = 0
+        self.trained_proportion = 0 
+
+        if self.training_type == 'finetune':
+            for name, param in self.model.named_parameters():
+                param.requires_grad = True
+            self.trained_proportion = 1
+        
+        elif self.training_type == 'frozen':
+            for name, param in self.model.named_parameters():
+                if 'embeddings' in name or 'pooler' in name:
+                    param.requires_grad = True
+                    self.trained_parameters += torch.numel(param)
+                else: param.requires_grad = False
+            self.trained_proportion = self.trained_parameters/self.trainable_params
+
+        elif self.training_type == 'optimized':
+            path = task+'-data.json' 
+            with open(path) as file: self.grad_dict = json.load(file)
+            var_dict = dict()
+            for key in self.grad_dict.keys(): var_dict[key] = torch.var(torch.tensor(self.grad_dict[key]))
+            sorted_var_dict = dict(sorted(var_dict.items(), key=lambda x: x[1]))
+            key_list, value_list = [], []
+            for key, value in sorted_var_dict.items():
+                value_list.append(value) 
+                key_list.append(key)
+            counter=0
+            cumulative_list = np.array(np.cumsum(value_list)/np.sum(value_list), dtype=np.float16)
+            for value in cumulative_list:
+                if value > 0.01: break
+                else: counter+=1
+            value=counter-1
+
+            for name, param in self.model.named_parameters(): 
+                    if name in key_list[value:] or ('embeddings' in name or 'pooler' in name):
+                        param.requires_grad = True
+                        self.trained_parameters += torch.numel(param)
+                    else: param.requires_grad = False
+            self.trained_proportion = self.trained_parameters/self.trainable_params
+        
+    def forward(self, 
+                input_ids, 
+                attention_mask
+                ):
+        
+        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output = outputs.pooler_output
+        logits = self.fc(pooled_output)
+        return logits
+
+    def get_loss(self, 
+                 logits, 
+                 labels
+                ):
+        
+        if self.task == 'stsb':
+            criterion = nn.MSELoss()
+        else:
+            criterion = nn.CrossEntropyLoss()
+        loss = criterion(logits, labels)
+        return loss
+    
+    def train_epoch(self, 
+                    dataloader, 
+                    optimizer, 
+                    scheduler,
+                    scheduler_updates,
+                    device
+                    ):
+        
+        self.train()
+        total_loss = 0.0
+        total_correct = 0
+        batch_count = 0 
+        batch_list = list()
+        time_list = list()
+        loss_list = list()
+        accuracy_list = list()
+
+        if scheduler == 'polynomial_decay':
+            from torch.optim.lr_scheduler import PolynomialLR
+            scheduler = PolynomialLR(optimizer, power=1.0, total_iters=scheduler_updates)
+        
+        for batch in tqdm.tqdm(dataloader):
+            start_time = time.time()
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            
+            optimizer.zero_grad()
+            logits = self.forward(input_ids, attention_mask)
+            loss = self.get_loss(logits, labels)
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            
+            total_loss += loss.item()
+            loss_list.append(loss.item())
+            predicted_labels = torch.argmax(logits, dim=1)
+            total_correct += (predicted_labels == labels).sum().item()  
+            accuracy_list.append((predicted_labels == labels).sum().item()/len(labels))
+            
+            for name, param in self.model.named_parameters():
+                if param.grad is None:
+                    continue
+                elif param.grad is not None:
+                    self.grad_dict[name].append(round(torch.norm(param.grad).item(), 3))
+            batch_count += 1
+            batch_list.append(batch_count)
+            end_time = time.time()
+            time_list.append(end_time - start_time)
+
+        accuracy = total_correct / len(dataloader.dataset)
+        
+        return total_loss / len(dataloader), accuracy, loss_list, accuracy_list, time_list, batch_list
+    
+    def test_epoch(self, 
+                   dataloader, 
+                   device
+                   ):
+        
+        self.eval()
+        total_loss = 0.0
+        total_correct = 0
+        total_predictions = 0
+        batch_count = 0
+        batch_list = list()
+        time_list = list()
+        loss_list = list()
+        accuracy_list = list()
+        
+        with torch.no_grad():
+            for batch in tqdm.tqdm(dataloader):
+                start_time = time.time()
+                input_ids = batch['input_ids'].to(device)
+                attention_mask = batch['attention_mask'].to(device)
+                labels = batch['labels'].to(device)
+                logits = self.forward(input_ids, attention_mask)
+                loss = self.get_loss(logits, labels)
+                
+                total_loss += loss.item()
+                loss_list.append(loss.item())
+                _, predicted_labels = torch.max(logits, dim=1)
+                total_correct += (predicted_labels == labels).sum().item()
+                accuracy_list.append((predicted_labels == labels).sum().item()/len(labels))
+                total_predictions += labels.size(0)
+                batch_count += 1
+                batch_list.append(batch_count)
+                end_time = time.time()
+                time_list.append(end_time - start_time)
+                
+        accuracy = total_correct / total_predictions
+
+        return total_loss / len(dataloader), accuracy, loss_list, accuracy_list, time_list, batch_list
+
+    def file_write(self):
+        file_name = f'{self.task}-data.json'
+        try:
+            file = open(file_name, 'x')
+            with open(file_name, 'w') as file:
+                json.dump(self.grad_dict, file, indent=4)
+        except:
+            with open(file_name, 'w') as file:
+                json.dump(self.grad_dict, file, indent=4)
+        return file_name
+
+    def calculate_stats(self, 
+                        var=True, 
+                        mean=False, 
+                        top_n=5
+                        ):
+        
+        if var:
+            var_dict = dict()
+            var_list = list()
+            for key in self.grad_dict.keys():
+                var_dict[key] = torch.var(torch.tensor(self.grad_dict[key])) 
+            var_dict = dict(sorted(var_dict.items(), key=lambda item: item[1]))
+            for item in list(reversed(var_dict.keys()))[:top_n]:
+                print(f'{item} => {var_dict[item]}')
+                var_list.append(var_dict[item])
+                if not mean: return var_list
+        if mean:
+            mean_dict = dict()
+            mean_list = list()
+            for key in self.grad_dict.keys():
+                mean_dict[key] = torch.mean(torch.tensor(self.grad_dict[key])) 
+            mean_dict = dict(sorted(mean_dict.items(), key=lambda item: item[1]))
+            for item in list(reversed(mean_dict.keys()))[:top_n]:
+                print(f'{item} => {mean_dict[item]}')
+                mean_list.append(mean_dict[item])
+                if not var: return mean_list
+        return var_list, mean_list        
