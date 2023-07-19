4893dcdcdc06eaa885989428c994e21dc1b5da32
Arav Dhoot <aravdhoot@gmail.com>
2023-07-19 22:05:53 +0530

    removing scheduler

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ def run_experiment (
     print(f'Learning Rate: {learning_rate} - Total Epochs: {epochs} - Batch Size: {batch_size}')
 
     for epoch in range(epochs):
-        train_loss, train_accuracy, train_loss_list, train_accuracy_list, train_time_list, train_step_list = model.train_epoch(train_dataloader, optimizer, lr_scheduler, device)
+        train_loss, train_accuracy, train_loss_list, train_accuracy_list, train_time_list, train_step_list = model.train_epoch(train_dataloader, optimizer, device)
         test_loss, test_accuracy, test_loss_list, test_accuracy_list, test_time_list, test_step_list = model.test_epoch(test_dataloader, device)
 
         for tr_loss, tr_accuracy, tr_time, tr_step in zip(train_loss_list, train_accuracy_list, train_time_list, train_step_list):
