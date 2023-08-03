0706986f103a34d1adc757f7d32fe5c187ac2842
Arav Dhoot <aravdhoot@gmail.com>
2023-08-03 10:31:02 +0530

    removing redundant code

--- main.py
+++ main.py
@@ -174,58 +174,6 @@ def run_experiment (
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=betas, eps=eps, weight_decay=weight_decay)
     wandb.watch(model, log='all')
 
-    if (task == 'stsb' or task == 'rte' or task == 'mrpc') and not sweep:
-        import torch.nn as nn
-        from dataset.mnlidataset import MNLIDataset
-
-        mnli_dataset = load_dataset('glue', 'mnli')
-        mnli_num_classes = 3
-        mnli_batch_size = 16
-
-        mnli_train_data = mnli_dataset['train']
-        mnli_test_data = mnli_dataset['validation_matched']
-        mnli_train_dataset = MNLIDataset(mnli_train_data, tokenizer)
-        mnli_test_dataset = MNLIDataset(mnli_test_data, tokenizer)
-
-        mnli_train_dataloader = DataLoader(mnli_train_dataset, batch_size=mnli_batch_size, shuffle=True)
-        mnli_test_dataloader = DataLoader(mnli_test_dataset, batch_size=mnli_batch_size, shuffle=False)
-
-        model = Model(num_classes=mnli_num_classes, task=task, training_type=training_type, dropout=dropout).to(device)
-        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=betas, eps=eps, weight_decay=weight_decay)
-
-        train_loss, train_accuracy, train_loss_list, train_accuracy_list, train_time_list, train_step_list = model.train_epoch(mnli_train_dataloader, optimizer, device, 2, warmup_ratio=0.06)
-        test_loss, test_accuracy, test_loss_list, test_accuracy_list, test_time_list, test_step_list = model.test_epoch(mnli_test_dataloader, device)
-
-        for epoch in range(2):
-            for tr_loss, tr_accuracy, tr_time, tr_step in zip(train_loss_list, train_accuracy_list, train_time_list, train_step_list):
-                wandb.log(
-                    {
-                        'Train Loss':tr_loss,
-                        'Train Accuracy':tr_accuracy,
-                        'Train Time':tr_time,
-                        'Train Step': (epoch * len(train_step_list)) + tr_step
-                    }, 
-                )
-            
-            for te_loss, te_accuracy, te_time, te_step in zip(test_loss_list, test_accuracy_list, test_time_list, test_step_list):
-                wandb.log(
-                    {
-                        'Test Loss (batch)':te_loss,
-                        'Test Accuracy (batch)':te_accuracy,
-                        'Test Time':te_time,
-                        'Test Step': (epoch * len(test_step_list)) + te_step 
-                    },   
-                )
-    
-            print(f'Epoch {epoch + 1} - Train Loss: {train_loss:.4f} - Train Accuracy: {train_accuracy:.4f} - Test Loss: {test_loss:.4f} - Test Accuracy: {test_accuracy:.4f}')
-
-            model.fc = nn.Sequential(
-                nn.Linear(model.config.hidden_size, model.config.hidden_size),
-                nn.GELU(),
-                dropout,
-                nn.Linear(model.config.hidden_size, num_classes)
-                )
-
     print(f'Learning Rate: {learning_rate} - Total Epochs: {epochs} - Batch Size: {batch_size}')
 
     for epoch in range(epochs):
