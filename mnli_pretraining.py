772bafd5b20feffdf7efd509a41d059e02864381
Arav Dhoot <aravdhoot@gmail.com>
2023-08-10 11:07:35 +0530

    remove unnecessary code blocks

--- mnli_pretraining.py
+++ mnli_pretraining.py
@@ -34,8 +34,7 @@ def run():
     model = Model(num_classes=mnli_num_classes, task='mnli', training_type='finetune', dropout=dropout).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
 
-    for epoch in range(1):
-        train_loss, train_accuracy, train_loss_list, train_accuracy_list, train_time_list, train_step_list = model.train_epoch(mnli_train_dataloader, optimizer, device, 2, warmup_ratio=0.06)
+    model.train_epoch(mnli_train_dataloader, optimizer, device, 2, warmup_ratio=0.06)
 
     tokenizer.save_pretrained(tokenizer_save_path)
     model.save_pretrained(model_save_path)
